545d618568b8ab6edc539f583909427084b3568e
Cory DeMille <cory@corydemille.com>
2014-01-21 01:45:56 -0700

    initial commit

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,6 @@
+<?php
+class HomeController extends BaseController {
+	public function home() {
+		return View::make('home');
+	}
+}
---
+++ app/routes.php
@@ -0,0 +1,5 @@
+<?php
+Route::get('/', array(
+	'as' => 'home',
+	'uses' => 'HomeController@home'
+));
